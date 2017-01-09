687f33204cb7849949503265bb1e19a57f9479c5
jaychaoqun <jaychaoaqun@126.com>
2017-01-09 15:42:23 +0800

    11

--- web3 Maven Webapp/src/mian/java/model/user.java
+++ web3 Maven Webapp/src/mian/java/model/user.java
@@ -2,6 +2,54 @@ package model;
 
 import java.io.Serializable;
 
-public class user implements Serializable {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table				//(name="t_user")
+public class user implements Serializable {
+	
+	private int id;
+	private String openid;
+	private String nickname;
+	private String avatar;
+	private String sex;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) { 
+		this.id = id;
+	}
+	public String getOpenid() {
+		return openid;
+	}
+	public void setOpenid(String openid) {
+		this.openid = openid;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getAvatar() {
+		return avatar;
+	}
+	public void setAvatar(String avatar) {
+		this.avatar = avatar;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	
+	
 }
